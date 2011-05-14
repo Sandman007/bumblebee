31b0fd67d52cb5cbd14f075b2dc9bbaf915a5a7f
Arnaud Dupuis <a.dupuis@infinityperl.org>
2011-05-14 05:29:08 +0200

    Minor bugfixe

--- install.sh
+++ install.sh
@@ -268,7 +268,6 @@ elif [ $DISTRO = FEDORA  ]; then
 cp install-files/bumblebee.script.fedora /etc/init.d/bumblebee
 cp -n /etc/bashrc /etc/bashrc.optiorig
 elif [ $DISTRO = OPENSUSE  ]; then
-cp install-files/bumblebee.script.openSUSE /etc/rc.d/bumblebee
 cp install-files/bumblebee.script.openSUSE /etc/init.d/bumblebee
 chmod +x /etc/init.d/bumblebee
 cp -n /etc/bash.bashrc /etc/bash.bashrc.optiorig
